29153b1a72004533b0c6e7954ffa5fdec8aae417
Sha <georcia.eugenio@edu.sait.ca>
2023-09-15 13:58:20 -0600

    Week 2 edit

---
+++ app/StudentInfo.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <p>Name: Sha Eugenio</p>
+      <p>Course Section: CPRG 306 C</p>
+      <Link href="https://github.com/ShaEugenio">GitHub Link: ShaEugenio</Link>
+    </main>
+  )
+}
---
+++ app/week2/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import StudentInfo from "../StudentInfo";
+
+export default function Week2()
+{
+    return (
+        <main>
+            <h1>My Shopping List</h1>
+            <StudentInfo />
+        </main>
+    );
+}
